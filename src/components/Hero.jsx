85c177c6da4df01f9b68881f122d8e71d7e6bd06
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-13 12:09:58 +0530

    feat: add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+const Hero = () => {
+  return (
+    <section className="w-full min-h-screen flex flex-col items-center justify-center uppercase">
+      <p className="font-editorial text-[3.8vw] leading-[4.8vw]">
+        <span className="font-vibes text-[1.5em] mr-2.5">o</span>
+        livier <span className="font-vibes text-[1.5em] mr-2.5">L</span>
+        arose
+      </p>
+      <p className="font-editorial text-[3.8vw] leading-[4.8vw]">
+        Independent Front End
+      </p>
+      <p className="font-mondwest text-[5vw] leading-[4.8vw]">☼Developer☀</p>
+      <p className="font-editorial text-[3.8vw] leading-[4.8vw]">
+        Having a <span className="font-vibes text-[1.5em] mr-2.5">G</span>
+        ood Time
+      </p>
+      <p className="font-editorial text-[3.8vw] leading-[4.8vw]">
+        Based In Montreal
+      </p>
+      <p className="font-editorial text-[3.8vw] leading-[4.8vw]">
+        Folio
+        <span className=" font-mondwest text-[1.5em]">©</span>
+        2026
+        <span className="font-mondwest text-[1.5em]">⚗✨</span>
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
