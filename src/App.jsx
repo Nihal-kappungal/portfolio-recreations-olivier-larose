0c7de75178e45607010e6b01c64afc3eecdf4041
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-19 11:50:39 +0530

    feat: hero grid animation implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Project from "./components/Project";
+import GridAnimation from "./components/GridAnimation";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Hero />
+      <GridAnimation />
       <Project />
     </div>
   );
