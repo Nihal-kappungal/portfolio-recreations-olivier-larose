0015c86e129a9a1ad7cc6dd69cdb18bf905d3490
Nihal-kappungal <nihalkappungal.workspace@gmail.com>
2026-09-20 13:17:04 +0530

    experiment: lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import Header from "./components/Header";
+import { useEffect } from "react";
+import Lenis from "lenis";
+
 import Project from "./components/Project";
 import GridAnimation from "./components/GridAnimation";
 import Loader from "./components/Loader";
 import Hero from "./components/Hero";
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+      lerp: 0.1,
+      smoothWheel: true,
+      respectReducedMotion: false,
+    });
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
-    <div>
+    <main>
       <Loader />
       <GridAnimation />
       <Hero />
       <Project />
-    </div>
+    </main>
   );
 };
 
